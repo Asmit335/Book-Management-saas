c6ffcedc896f1b6d7c7ed7a5b37c1e874bbf85af
Asmit Khanal <asmitkhanal335@gmail.com>
2026-07-06 08:01:16 +0900

    prepared for hosting in render

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+
 require("./database/connection");
 const bookRoute = require("./routes/bookRoutes");
 app.use(express.json());
 app.use(cors());
 
 app.use("/", bookRoute);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(3000, () => {
   console.log("Server is running in Port NO. 3000");
